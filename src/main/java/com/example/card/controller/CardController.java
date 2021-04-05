3865705e5e7a0b3c65bd7216de57edc18530115d
Mutlu Eren <42313363+mutlueren@users.noreply.github.com>
2021-04-05 20:13:43 +0300

    Update CardController.java

--- src/main/java/com/example/card/controller/CardController.java
+++ src/main/java/com/example/card/controller/CardController.java
@@ -3,11 +3,11 @@ package com.example.card.controller;
 import com.example.card.service.CreditCardMaskingService;
 import com.example.card.service.CreditCardValidatingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class CardController {
 
     @Autowired
